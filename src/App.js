06800b7805bb3e24962507b747a3ae3a2263272a
Suphawut Pitirit <suphawut@robowealth.co.th>
2020-11-09 00:29:56 +0700

    success gallery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  let [responseData, setResponseData] = React.useState('');
+
+  React.useEffect(() => {
+    axios({
+      "method": "GET",
+      "url": "https://picsum.photos/v2/list"
+    })
+    .then((response) => {
+      setResponseData(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
+  const generateImageViews = (item) => <img height="150" className="Image" key={item.id} src={item.download_url}/>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div>Everyone's photos</div>
+        <div style={{color: 'dodgerblue'}}>View all 2,724,700</div>
+      </div>
+      <div className="Image-Content">
+      {responseData ? responseData.map(v => generateImageViews(v)): null}
+      </div>
     </div>
   );
 }
